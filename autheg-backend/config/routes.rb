ff77eafb40016cb671e540c3745cbdf4706db577
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2020-04-10 22:14:18 +0900

    all

--- autheg-backend/config/routes.rb
+++ autheg-backend/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  scope :api, defaults: {format: :json} do
+    resources :examples
+  end
 end
